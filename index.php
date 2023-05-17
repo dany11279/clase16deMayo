c17f780b9e3738b1517402bb6101ff9618e05759
Dany <danymel1279@gmail.com>
2023-05-16 18:23:37 -0600

    conexion BD.

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php   
+
+try {
+    $conexion= new PDO ("informix:host=host.docker.internal; service=9088;database=mdn;
+     server=informix; protocol=onsoctcp;EnableScrollableCursors=1", "informix","in4mix");
+    echo "conexion exitosa";
+} catch (PDOException $e){
+    echo "CONEXION A LA BASE DE DATOS MIRON_M";
+    echo "<br>";
+    echo $e->getMessage ();
+}
+echo "<br>";
+echo "Mensaje despues de la conexion";
